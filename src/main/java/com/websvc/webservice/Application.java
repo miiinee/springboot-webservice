d585913acecacad949092a2e5e1d5c40302abc81
miiinee <psmopen9357@gmail.com>
2019-04-11 16:14:51 +0900

    - profile 적용

--- src/main/java/com/websvc/webservice/Application.java
+++ src/main/java/com/websvc/webservice/Application.java
@@ -1,7 +1,8 @@
 package com.websvc.webservice;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Auditing 활성화
@@ -14,7 +15,10 @@ public class Application {
 
 	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+							        .properties(APPLICATION_LOCATIONS)
+							        .run(args);
+//		SpringApplication.run(Application.class, args);
 	}
 
 }
