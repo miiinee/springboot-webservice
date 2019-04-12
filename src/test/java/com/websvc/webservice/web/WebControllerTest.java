ce4d2282d65be018b3987a3f449b20cebf8a8979
miiinee <psmopen9357@gmail.com>
2019-04-12 10:04:58 +0900

    - 운영db 연동

--- src/test/java/com/websvc/webservice/web/WebControllerTest.java
+++ src/test/java/com/websvc/webservice/web/WebControllerTest.java
@@ -21,7 +21,7 @@ public class WebControllerTest {
 	public void 메인페이지_로딩() {
 		//when
         String body = this.restTemplate.getForObject("/", String.class);
-
+        
         //then
         assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
 	}
